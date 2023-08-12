1f0dd2a368072b73de3e4b87df119d960fbb79ce
idrisslongo <idrisslongo@outlook.com>
2023-08-12 21:13:59 +0200

    Documentation du point d'entrée

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace MaBanque_1._0._0
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form1()); // Point d'entréé de l'application
         }
     }
 }
